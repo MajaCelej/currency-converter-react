8b3a6f22269c2f416b4e4a4206f7b6074760baac
Maja Celej <cmaja1825@gmail.com>
2024-01-16 19:16:15 +0100

    adjusted code, based on moving it to different folders

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import Text from "./Text";
 import Container from "./Container";
-import Button from "./Button";
+import Form from "./Form";
 import Outcome from "./Outcome";
-import Input from "./Input";
-import Label from "./Label";
-import Select from "./Select";
 import { useState } from "react";
 
 function App() {
   const currencies = [
     { id: 1, name: "Euro", symbol: "EUR", rate: 4.34 },
-    { id: 2, name: "Frank Szwajcarski", symbol: "CHF", rate: 4.64},
+    { id: 2, name: "Frank Szwajcarski", symbol: "CHF", rate: 4.64 },
     { id: 3, name: "Korona Czeska", symbol: "CZK", rate: 0.17 },
   ];
 
@@ -20,46 +17,34 @@ function App() {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-  }
+  };
 
   const calculateResult = () => {
     const selectedCurrency = currencies.find(({ name }) => name === currency);
 
-    if (!selectedCurrency ) {
-      console.error("Currency not found")
+    if (!selectedCurrency) {
+      console.error("Currency not found");
       return;
-    }
-    else {
-      setResult(amount * selectedCurrency.rate)
+    } else {
+      setResult(amount * selectedCurrency.rate);
     }
   };
 
   return (
-  <Container>
-    <Text />
-    <form className="form" onSubmit={onFormSubmit}>
-      <div>
-        <Label
-         className="form__label"
-         title={"Kwota:"}
-        />
-        <Input 
-         setAmount={setAmount} 
-         amount={amount}
-        />
-      </div>
-      <div>
-        <Label title={"Waluta:"}/>
-        <Select 
-         setCurrency={setCurrency}
-         currency={currency}
-        />
-      </div>
-      <Button calculateResult={calculateResult} />
-    </form>
+    <Container>
+      <Text />
+      <Form 
+        onFormSubmit={onFormSubmit}
+        setCurrency={setCurrency}
+        setAmount={setAmount}
+        amount={amount}
+        currency={currency}
+        calculateResult={calculateResult}
+        setResult={setResult}
+      />
       <Outcome result={result} />
-  </Container>
+    </Container>
   );
-};
+}
 
 export default App;
