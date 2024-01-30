e2bbb8a25d17b4bc6dd93525a1b14a25123070ae
Maja Celej <cmaja1825@gmail.com>
2024-01-30 18:19:02 +0100

    modified code in Container folder, transitioned css to styled.js

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,8 +1,10 @@
-.container {
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
     max-width: 700px;
     margin: 0 auto;
     padding: 0 20px;
     font-family: 'Niramit', sans-serif;
     display: flex;
     flex-direction: column;
-}
+`;
