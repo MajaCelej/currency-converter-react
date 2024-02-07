12c710121e8bb554e184a7f04be35bdac8a7e4b9
Maja Celej <cmaja1825@gmail.com>
2024-02-07 19:03:17 +0100

    moved logic to form index.js

--- src/App.js
+++ src/App.js
@@ -6,49 +6,15 @@ import Clock from "./Clock";
 import { useState } from "react";
 
 function App() {
-  const currencies = [
-    { id: 1, name: "Euro", symbol: "EUR", rate: 4.34 },
-    { id: 2, name: "Frank Szwajcarski", symbol: "CHF", rate: 4.64 },
-    { id: 3, name: "Korona Czeska", symbol: "CZK", rate: 0.17 },
-  ];
 
-  const [currency, setCurrency] = useState(currencies[0].name);
-  const [amount, setAmount] = useState("");
-  const [result, setResult] = useState("");
-
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-  };
-
-  const calculateResult = () => {
-    const selectedCurrency = currencies.find(({ name }) => name === currency);
-
-    if (!selectedCurrency) {
-      console.error("Currency not found")
-      return;
-    } 
-    
-    else {
-      setResult(amount * selectedCurrency.rate)
-    }
-  };
-
-  return (
-    <Container>
-      <Text />
-      <Clock />
-      <Form 
-        onFormSubmit={onFormSubmit}
-        setCurrency={setCurrency}
-        setAmount={setAmount}
-        amount={amount}
-        currency={currency}
-        calculateResult={calculateResult}
-        setResult={setResult}
-      />
-      <Outcome result={result} />
-    </Container>
-  );
-};
+	return (
+		<Container>
+			<Text />
+			<Clock />
+			<Form />
+			<Outcome />
+		</Container>
+	);
+}
 
 export default App;
