8493a3d0207e9a1be458ba7afd99a4e6757eadea
Maja Celej <cmaja1825@gmail.com>
2024-02-07 19:00:15 +0100

    formatted code

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -2,23 +2,19 @@ import { Timer } from "./styled";
 import { useCurrentDate } from "./useCurrentDate";
 
 const Clock = () => {
-
-    const formatDate = (date) => date.toLocaleString(undefined, {
-        weekday: "long",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        day: "numeric",
-        month: "long",
-    });
+    const formatDate = (date) =>
+        date.toLocaleString(undefined, {
+            weekday: "long",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+            day: "numeric",
+            month: "long",
+        });
 
     const date = useCurrentDate();
 
-    return (
-        <Timer>
-            Dzisiejsza data: {formatDate(date)}
-        </Timer>
-    )
+    return <Timer>Dzisiejsza data: {formatDate(date)}</Timer>;
 };
 
 export default Clock;
--- src/Container/styled.js
+++ src/Container/styled.js
@@ -4,7 +4,7 @@ export const Wrapper = styled.div`
     max-width: 700px;
     margin: 0 auto;
     padding: 0 20px;
-    font-family: 'Niramit', sans-serif;
+    font-family: "Niramit", sans-serif;
     display: flex;
     flex-direction: column;
 `;
