46647cd888e830aa612ece13fb491c1304104582
Maja Celej <cmaja1825@gmail.com>
2024-02-12 11:05:05 +0100

    moved Outcome to Form folder

--- src/Outcome/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.section`
-    background-color: ${(props) => props.theme.color.waxflower};
-    border: solid 1px ${(props) => props.theme.color.black};
-    text-align: center;
-`;
