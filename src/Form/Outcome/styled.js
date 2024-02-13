2ca67c48f167bbb02f988d18179fd7a26d759c8e
Maja Celej <cmaja1825@gmail.com>
2024-02-13 19:51:37 +0100

    adjusted outcome style

--- src/Form/Outcome/styled.js
+++ src/Form/Outcome/styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.section`
-    background-color: ${(props) => props.theme.color.waxflower};
-    border: solid 1px ${(props) => props.theme.color.black};
     text-align: center;
+    font-size: large;
 `;
